7e42f27f03078ab675fc72c0be0ee3a087bacbef
pqppq <yutatsu0119@gmail.com>
2022-12-29 02:12:20 +0900

    feat: impl db.Latest

--- snippetbox/internal/models/snippets.go
+++ snippetbox/internal/models/snippets.go
@@ -57,5 +57,26 @@ func (m *SnippetModel) Get(id int) (*Snippet, error) {
 }
 
 func (m *SnippetModel) Latest() ([]*Snippet, error) {
-	return nil, nil
+	stmt := `SELECT id, title, content, created, expires FROM snippets WHERE expires > UTC_TIMESTAMP() ORDER BY id DESC LIMIT 10`
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+
+	// close DB connection after use
+	defer rows.Close()
+
+	snippets := []*Snippet{}
+
+	// scan rows
+	for rows.Next() {
+		s := &Snippet{}
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+		snippets = append(snippets, s)
+	}
+
+	return snippets, nil
 }
