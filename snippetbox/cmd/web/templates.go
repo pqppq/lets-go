8dda01a9cb147260e0b023a94ecb76e975441b82
pqppq <yutatsu0119@gmail.com>
2022-12-29 21:49:54 +0900

    feat: add helper func for human readable

--- snippetbox/cmd/web/templates.go
+++ snippetbox/cmd/web/templates.go
@@ -3,6 +3,7 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"github.com/pqppq/lets-go/snippetbox/internal/models"
 )
@@ -11,6 +12,10 @@ type templateData struct {
 	CurrentYear int
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
@@ -24,7 +29,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
@@ -43,4 +48,17 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	}
 
 	return cache, nil
+}
+
+func humanDate(t time.Time) string {
+	// 1: month (January, Jan, 01, etc)
+	// 2: day
+	// 3: hour (15 is 3pm on a 24 hour clock)
+	// 4: minute
+	// 5: second
+	// 6: year (2006)
+	// 7: timezone (GMT-7 is MST)
+
+	// DD Mon YYYY as HH:mm
+	return t.Format("02 Jan 2006 at 15:04")
 }
