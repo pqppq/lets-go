613d691f573c9f6528536f79becb25d5fc997765
pqppq <yutatsu0119@gmail.com>
2023-01-01 22:10:50 +0900

    feat: add UserModel.Insert

--- snippetbox/internal/models/users.go
+++ snippetbox/internal/models/users.go
@@ -2,7 +2,12 @@ package models
 
 import (
 	"database/sql"
+	"errors"
+	"strings"
 	"time"
+
+	"github.com/go-sql-driver/mysql"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -23,4 +28,26 @@ func (m *UserModel) Authenticate(email, password string) (int, error) {
 }
 func (m *UserModel) Exists(id int) (bool, error) {
 	return false, nil
+}
+
+func (m *UserModel) Insert(name, email, password string) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+
+	stmt := `INSERT INTO users (name, email, hashed_password, created)
+		VALUES(?, ?, ?, UTC_TIMESTAMP())`
+	_, err = m.DB.Exec(stmt, name, email, string(hashedPassword))
+	if err != nil {
+		var mySQLError *mysql.MySQLError
+		if errors.As(err, &mySQLError) {
+			if mySQLError.Number == 1062 && strings.Contains(mySQLError.Message, "users_uc_email") {
+				// error code:1062 ER_DUP_ENTRY
+				return ErrDuplidatedEmail
+			}
+		}
+		return err
+	}
+	return nil
 }
